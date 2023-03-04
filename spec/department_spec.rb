929728daa37440baddcae7b115bae21374074181
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-04 13:35:00 -0700

    Add tests for department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -6,12 +6,29 @@ RSpec.describe Employee do
   before(:each) do
     @customer_service = Department.new("Customer Service")
     @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+    @aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
   end
 
   describe 'department' do
     it 'exists and has attributes' do
       expect(@customer_service).to be_a(Department)
       expect(@customer_service.name).to eq('Customer Service')
+      expect(@customer_service.employees).to eq([])
+      expect(@customer_service.expenses).to eq(0)
+    end
+
+    it 'can add employees' do
+      @customer_service.hire(@bobbi)
+      @customer_service.hire(@aaron)
+
+      expect(@customer_service.employees).to eq([@bobbi, @aaron])
+    end
+
+    it 'can add expenses' do
+      @customer_service.expense(100)
+      @customer_service.expense(25)
+
+      expect(@customer_service.expenses).to eq(125)
     end
   end
 end
